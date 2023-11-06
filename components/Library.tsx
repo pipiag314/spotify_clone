9c8bf7f43bf1ac0bead54789124e545951020ea2
gela pipia <gelapipia10@gmail.com>
2023-11-06 15:37:19 +0400

    finished sidebar markup

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,33 +1,59 @@
 "use client";
 
-import { TbPlaylist } from "react-icons/tb"
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
 
 const Library = () => {
+  const onClick = () => {
+    // handle upload for library
+    console.log("new library created");
+  };
 
-    const onClick = () => {
-        // handle upload for library
-    }
-    
-    return (
-        <div className="flex flex-col">
-            <div className="
+  return (
+    <div className="flex flex-col">
+      <div
+        className="
                 flex
                 items-center
                 justify-between
                 px-5
                 pt-4
             ">
-                <div className="
+        <div
+          className="
                     inline-flex
                     items-center
                     gap-x-2
                 ">
-                    <TbPlaylist className="text-neutral-400" size={20} />
-
-                </div>
-            </div>
+          <TbPlaylist className="text-neutral-400" size={20} />
+          <p
+            className="
+                        text-neutral-400
+                        font-medium
+                        text-sm
+                    ">
+            Your Library
+          </p>
         </div>
-    )
-}
+        <AiOutlinePlus
+          className="cursor-pointer text-neutral-400 hover:text-white transition"
+          size={20}
+          onClick={onClick}
+        />
+      </div>
+      <div 
+        className="
+            flex
+            flex-col
+            gap-y-2
+            mt-4
+            px-3
+        "
+      >
+        List of songs
+      </div>
+    </div>
+  );
+};
 
 export default Library;
